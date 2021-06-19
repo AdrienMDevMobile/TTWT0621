422c9c1f090f6b4919ca144d715c73e0095fe34f
MICHEL Adrien <privateadrienps@gmail.com>
2021-06-19 15:51:00 +0200

    Retenir qui a été cliqué ou non.

--- app/src/main/java/com/example/ttwt0621/ImagePreviewAdapter.kt
+++ app/src/main/java/com/example/ttwt0621/ImagePreviewAdapter.kt
@@ -14,18 +14,16 @@ import kotlin.properties.Delegates
 
 
 class ImagePreviewAdapter(private val list : ArrayList<ImagePreview>, private val myImageLoader: MyImageLoader)
-    : RecyclerView.Adapter<ImagePreviewAdapter.ViewHolder>(){
+    : CBClicked, RecyclerView.Adapter<ImagePreviewAdapter.ViewHolder>()
+     {
     //private val context: Context,
     private val layoutInflater: LayoutInflater? = null
-
-   // companion object isChecked = ArrayList<Boolean>()
-
 
     override fun  onCreateViewHolder(parent: ViewGroup, viewType : Int) : ViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_image_list, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(view, this)
     }
 
     override fun getItemCount(): Int {
@@ -37,9 +35,10 @@ class ImagePreviewAdapter(private val list : ArrayList<ImagePreview>, private va
         holder.textImagePreview.setText(imagePreview.tags)
         holder.currentPosition = position
         myImageLoader.loadImage(holder.image, imagePreview.previewURL)
+        holder.checkBox.isChecked = imagePreview.isChecked
     }
 
-    class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView : View, viewHolder:CBClicked): RecyclerView.ViewHolder(itemView){
 
         val textImagePreview : TextView
         val image : NetworkImageView
@@ -50,10 +49,19 @@ class ImagePreviewAdapter(private val list : ArrayList<ImagePreview>, private va
             textImagePreview = itemView.findViewById(R.id.tvImagePreview);
             image = itemView.findViewById(R.id.img_preview)
             checkBox = itemView.findViewById(R.id.cBImagePreview)
-            /*
             checkBox.setOnCheckedChangeListener{view, checked ->
-
-            }*/
+                viewHolder.CBClicked(currentPosition, checked)
+            }
         }
+
     }
+
+     override fun CBClicked(position: Int, value: Boolean) {
+         list.get(position).isChecked = value
+     }
+
+
+     }
+interface CBClicked{
+    fun CBClicked(position : Int, value:Boolean)
 }
--- app/src/main/java/com/example/ttwt0621/data/ImagePreview.kt
+++ app/src/main/java/com/example/ttwt0621/data/ImagePreview.kt
@@ -3,5 +3,6 @@ package com.example.ttwt0621.data
 data class ImagePreview (
     val id:Int,
     val tags : String,
-    val previewURL : String
+    val previewURL : String,
+    var isChecked : Boolean = false
 )
